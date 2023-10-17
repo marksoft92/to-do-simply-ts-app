7a29ed0aef20653eac33c45ad75bf803fad9a68d
Dawid BIeńkowski <marksoft1992@gmail.com>
2023-10-17 20:21:14 +0200

    Added fetch and refactor slice for get list of Tasks

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { RouteProps } from "react-router-dom";
-
+import TasksList from "./containers/Tasks";
 
 const routes: RouteProps[] = [
   {
     path: "/",
     element: (
-      <div>"xx"</div>
+      <TasksList />
     ),
   },
 ];
