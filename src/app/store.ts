24bd2db90eab86ee4980c9a6851392f2a05adab8
Dawid BIeńkowski <marksoft1992@gmail.com>
2023-10-17 19:29:41 +0200

    Added new folder with slice for reducer Tasks

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,5 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-
+import tasksReducer from "../containers/Tasks/tasksSlice";
 export const store = configureStore({
   reducer: {
 
