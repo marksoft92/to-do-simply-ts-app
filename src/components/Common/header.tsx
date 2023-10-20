41a1bda07ea52de087bfcf959d23fb836d3d82a8
Dawid BIeńkowski <marksoft1992@gmail.com>
2023-10-20 11:45:33 +0200

    added header components with my profiles

---
+++ src/components/Common/header.tsx
@@ -0,0 +1,30 @@
+import React, { FC, memo } from 'react';
+import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
+import styles from "../../assets/styles/components/common/header.module.scss";
+const githubUrl = "https://www.github.com/marksoft92";
+const linkedinUrl = "https://www.linkedin.com/in/dawid-bie%C5%84kowski/";
+
+
+const Header: FC = () => {
+    return (
+        <header className={`${styles["header"]} flex align-items-center justify-content-center`}>
+            <h1>Dawid Bieńkowski</h1>
+            <a
+                href={githubUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <GithubOutlined />
+            </a>
+            <a
+                href={linkedinUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <LinkedinOutlined />
+            </a>
+        </header>
+    );
+};
+
+export default memo(Header);
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { RouteProps } from "react-router-dom";
 import TasksList from "./containers/Tasks";
+import Header from "./components/Common/header";
 
 const routes: RouteProps[] = [
   {
     path: "/",
     element: (
-      <TasksList />
+      <>
+        <Header />
+        <TasksList />
+      </>
     ),
   },
 ];
