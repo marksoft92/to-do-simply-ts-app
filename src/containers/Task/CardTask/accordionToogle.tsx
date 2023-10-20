ec51d9e28515340f859eaac32ca6f7e564dc6cf9
Dawid BIeńkowski <marksoft1992@gmail.com>
2023-10-20 11:25:41 +0200

    fixed error in console of website

--- src/containers/Task/CardTask/accordionToogle.tsx
+++ src/containers/Task/CardTask/accordionToogle.tsx
@@ -13,6 +13,10 @@ const AccordionToogle: FC<AccordionToogleProps> = ({
             type="radio"
             name={name}
             checked={checkedStatus}
+            onChange={() => {
+                // Non-standard use: Switching radio buttons on mouse enter and leave.
+                // This is done intentionally for mouse-based interaction.
+            }}
         />
     );
 };
